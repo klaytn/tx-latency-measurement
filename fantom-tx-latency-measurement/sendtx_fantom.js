6bb68d2709dacd613a9bee6bd6f71b85a2ca2be5
Yeonju-Kim <krust@krustui-MacBookPro.local>
2022-05-11 14:57:52 +0900

    Fantom tx latency measurement using Web3.js.

---
+++ fantom-tx-latency-measurement/sendtx_fantom.js
@@ -0,0 +1,199 @@
+// Fantom transaction latency measurement. 
+// Reference of Sending Transaction using Javascript: https://github.com/Fantom-foundation/fantom-web3-wallet/blob/master/index.js
+
+const Web3 = require('web3');
+const fs = require('fs')
+const AWS = require('aws-sdk')
+var parquet = require('parquetjs-lite')
+const moment = require('moment');
+const axios = require('axios');
+const CoinGecko = require('coingecko-api');
+const CoinGeckoClient = new CoinGecko(); 
+
+require('dotenv').config();
+
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.PUBLIC_RPC_URL_WEB3));
+const privateKey = process.env.SIGNER_PRIVATE_KEY;
+var PrevNonce = 0; 
+
+async function makeParquetFile(data) {
+    var schema = new parquet.ParquetSchema({
+        executedAt:{type:'TIMESTAMP_MILLIS'},
+        txhash:{type:'UTF8'},
+        startTime:{type:'TIMESTAMP_MILLIS'},
+        endTime:{type:'TIMESTAMP_MILLIS'},
+        chainId:{type:'INT64'},
+        latency:{type:'INT64'},
+        error:{type:'UTF8'},
+        txFee:{type:'DOUBLE'},
+        txFeeInUSD:{type:'DOUBLE'},
+        resourceUsedOfLatestBlock:{type:'INT64'},
+        numOfTxInLatestBlock:{type:'INT64'},
+        pingTime:{type:'INT64'}
+    })
+  
+    var d = new Date()
+    //20220101_032921
+    var datestring = moment().format('YYYYMMDD_HHmmss')
+  
+    var filename = `${datestring}_${data.chainId}.parquet`
+  
+    // create new ParquetWriter that writes to 'filename'
+    var writer = await parquet.ParquetWriter.openFile(schema, filename);
+  
+    await writer.appendRow(data)
+  
+    writer.close()
+  
+    return filename;
+}
+  
+async function sendSlackMsg(msg) {
+    axios.post(process.env.SLACK_API_URL, {
+        'channel':process.env.SLACK_CHANNEL,
+        'mrkdown':true,
+        'text':msg
+    }, {
+        headers: {
+            'Content-type':'application/json',
+            'Authorization':`Bearer ${process.env.SLACK_AUTH}`
+        }
+    })
+}
+  
+async function uploadToS3(data){
+    const s3 = new AWS.S3();
+    const filename = await makeParquetFile(data)
+    const param = {
+        'Bucket':process.env.S3_BUCKET,
+        'Key':filename,
+        'Body':fs.createReadStream(filename),
+        'ContentType':'application/octet-stream'
+    }
+    await s3.upload(param).promise()
+  
+    fs.unlinkSync(filename) 
+}
+
+async function sendTx(){
+    var data = {
+        executedAt: new Date().getTime(),
+        txhash: '',
+        startTime: 0,
+        endTime: 0,
+        chainId: 0,
+        latency:0,
+        error:'',
+        txFee: 0.0, 
+        txFeeInUSD: 0.0, 
+        resourceUsedOfLatestBlock: 0,
+        numOfTxInLatestBlock: 0,
+        pingTime:0 
+    }
+    
+    try{
+        const signer = web3.eth.accounts.privateKeyToAccount(
+            process.env.SIGNER_PRIVATE_KEY
+        );
+        const balance = await web3.eth.getBalance(signer.address)
+
+        if(balance*(10**(-18)) < parseFloat(process.env.BALANCE_ALERT_CONDITION_IN_FTM))
+        {
+            sendSlackMsg(`Current balance of <${process.env.SCOPE_URL}/address/${signer.address}|${signer.address}> is less than ${process.env.BALANCE_ALERT_CONDITION_IN_FTM} FTM! balance=${balance*(10**(-18))} FTM`)
+        }
+
+        await web3.eth.net.getId().then((id)=>{
+            data.chainId = id
+        })
+        const gasPrice = await web3.eth.getGasPrice();
+        const latestNonce = await web3.eth.getTransactionCount(signer.address);
+        if (latestNonce == PrevNonce) 
+        {
+          console.log(`Nonce ${latestNonce} = ${PrevNonce}`)
+          return;
+        }
+
+        const startGetBlock = new Date().getTime()
+        const latestBlockNumber = await web3.eth.getBlockNumber();
+        const endGetBlock = new Date().getTime()
+        data.pingTime = endGetBlock - startGetBlock
+
+        await web3.eth.getBlock(latestBlockNumber).then((blockInfo)=>{
+            data.resourceUsedOfLatestBlock = blockInfo.gasUsed
+            data.numOfTxInLatestBlock = blockInfo.transactions.length
+        })
+
+        const rawTx = {
+            from: signer.address, 
+            to: signer.address,
+            value: Web3.utils.toHex(Web3.utils.toWei("0", 'ether')),
+            gasLimit: Web3.utils.toHex(21000),
+            gasPrice: Web3.utils.toHex(gasPrice),
+            nonce: Web3.utils.toHex(latestNonce)
+        }
+
+        var RLPEncodedTx;
+        await web3.eth.accounts.signTransaction(rawTx, privateKey)
+        .then((result)=>
+        {   
+            RLPEncodedTx = result.rawTransaction // RLP encoded transaction & already HEX value
+            data.txhash = result.transactionHash // the transaction hash of the RLP encoded transaction.
+        })
+        
+        const originalPrevNonce = PrevNonce
+        // Send signed transaction
+        const start = new Date().getTime()
+        data.startTime = start
+        await web3.eth
+        .sendSignedTransaction(RLPEncodedTx)
+        .on('sent', function(){
+            PrevNonce = latestNonce
+        })
+        .on('receipt', function(receipt){
+            data.txhash = receipt.transactionHash
+            const end = new Date().getTime()
+            data.endTime = end
+            data.latency = end-start
+            data.txFee = receipt.gasUsed * web3.utils.fromWei(receipt.effectiveGasPrice.toString())
+        })
+        .on('error', function(err){
+            PrevNonce = originalPrevNonce
+        })
+
+        // Calculate Transaction Fee and Get Tx Fee in USD 
+        var FTMtoUSD;
+        await CoinGeckoClient.simple.price({
+            ids: ["fantom"],
+            vs_currencies: ["usd"]
+        }).then((response)=>{
+            FTMtoUSD = response.data["fantom"]["usd"]
+        })
+        data.txFeeInUSD = data.txFee * FTMtoUSD
+        
+        console.log(`${data.executedAt},${data.chainId},${data.txhash},${data.startTime},${data.endTime},${data.latency},${data.txFee},${data.txFeeInUSD},${data.resourceUsedOfLatestBlock},${data.numOfTxInLatestBlock},${data.pingTime},${data.error}`)
+    } catch(err){
+        console.log("failed to execute.", err.toString())
+        data.error = err.toString()
+        console.log(`${data.executedAt},${data.chainId},${data.txhash},${data.startTime},${data.endTime},${data.latency},${data.txFee},${data.txFeeInUSD},${data.resourceUsedOfLatestBlock},${data.numOfTxInLatestBlock},${data.pingTime},${data.error}`)
+    }
+    try{
+        await uploadToS3(data)
+    } catch(err){
+        console.log('failed to s3.upload', err.toString())
+    }
+
+}
+
+async function main(){
+    const start = new Date().getTime()
+    console.log(`starting tx latency measurement... start time = ${start}`)
+
+    // run sendTx every SEND_TX_INTERVAL
+    const interval = eval(process.env.SEND_TX_INTERVAL)
+    setInterval(()=>{
+      sendTx()
+    }, interval)
+  
+}
+  
+main();
