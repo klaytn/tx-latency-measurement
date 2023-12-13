0a4af61f964b125f7417c603e79b239387f45e26
ara-klaytn <palani.aravindh@klaytn.foundation>
2023-12-13 16:05:42 +0800

    fix spelling mistake

--- harmony-tx-latency-measurement/sendtx_harmony.js
+++ harmony-tx-latency-measurement/sendtx_harmony.js
@@ -224,7 +224,7 @@ async function sendTx() {
     data.txFeeInUSD = data.txFee * ONEtoUSD;
     // console.log(`${data.executedAt},${data.chainId},${data.txhash},${data.startTime},${data.endTime},${data.latency},${data.txFee},${data.txFeeInUSD},${data.resourceUsedOfLatestBlock},${data.numOfTxInLatestBlock},${data.pingTime},${data.error}`)
   } catch (err) {
-    sendSlackMsg(`Faield to execute, ${err.toString()}`);
+    sendSlackMsg(`Failed to execute, ${err.toString()}`);
     console.log("failed to execute.", err.toString());
     data.error = err.toString();
     // console.log(`${data.executedAt},${data.chainId},${data.txhash},${data.startTime},${data.endTime},${data.latency},${data.txFee},${data.txFeeInUSD},${data.resourceUsedOfLatestBlock},${data.numOfTxInLatestBlock},${data.pingTime},${data.error}`)
